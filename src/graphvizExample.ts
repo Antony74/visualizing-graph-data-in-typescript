3a10a998c16454648d3a2b6a4ac30a5291b62d49
Antony74 <akb@akb.me.uk>
2023-12-19 22:05:46 +0000

    Add link labels to graphvizExample

--- src/graphvizExample.ts
+++ src/graphvizExample.ts
@@ -1,16 +1,20 @@
 import fs from 'fs/promises';
+import path from 'path';
+
 import graphviz from 'graphviz';
 import { graphviz as nodegraphviz } from 'node-graphviz';
 
 const graph = graphviz.digraph('Example');
 
-graph.addEdge('loose-envify', 'react');
-graph.addEdge('loose-envify', 'react-dom');
-graph.addEdge('scheduler', 'react-dom');
-graph.addEdge('regenerator-runtime', '@babel/runtime');
-graph.addEdge('@babel/runtime', 'redux');
-graph.addEdge('react', 'react-redux');
-graph.addEdge('react-dom', 'react-redux');
-graph.addEdge('redux', 'react-redux');
+graph.addEdge('loose-envify', 'react', { label: 1 });
+graph.addEdge('loose-envify', 'react-dom', { label: 2 });
+graph.addEdge('scheduler', 'react-dom', { label: 1 });
+graph.addEdge('regenerator-runtime', '@babel/runtime', { label: 1 });
+graph.addEdge('@babel/runtime', 'redux', { label: 1 });
+graph.addEdge('react', 'react-redux', { label: 1 });
+graph.addEdge('react-dom', 'react-redux', { label: 1 });
+graph.addEdge('redux', 'react-redux', { label: 1 });
 
-nodegraphviz.layout(graph.to_dot(), 'svg').then(svg => fs.writeFile('graphvizExample.svg', svg));
+const filename = path.join(__dirname, 'graphvizExample.svg');
+
+nodegraphviz.layout(graph.to_dot(), 'svg').then(svg => fs.writeFile(filename, svg));
